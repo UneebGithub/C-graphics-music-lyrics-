074f1d0c9ad1ac26285f1524f017f48ef0ad9375
UneebGithub <96345925+UneebGithub@users.noreply.github.com>
2022-09-04 00:56:13 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include<graphics.h>
+#include<windows.h>
+#include<mmsystem.h>
+
+
+int main()
+{
+	//music 
+	//PlaySound(TEXT(" p .wav"),NULL,SND_SYNC);
+
+    initwindow(600,500,"TUTORIALdd");
+    settextstyle(EUROPEAN_FONT,HORIZ_DIR,5);
+    outtextxy(150,155,"Woh");
+    setcolor(YELLOW);
+    delay(1000);
+    cleardevice();
+    outtextxy(152,150,"Hai");
+    delay(1000);
+    setcolor(RED);
+    cleardevice();
+    outtextxy(154,150,"Tu");
+    delay(1350);
+       cleardevice();
+       setcolor(LIGHTMAGENTA);
+	   outtextxy(150,155,"Pyar");
+	   delay(800);
+	   cleardevice();
+    setcolor(LIGHTRED);
+	   outtextxy(155,155,"Hai");
+	   delay(700);
+	   cleardevice();
+       setcolor(BLUE);
+	   outtextxy(150,155,"Tu");
+	   delay(700);
+	   outtextxy(150,155,"Woh");
+    setcolor(LIGHTCYAN);
+    delay(700);
+    cleardevice();
+    outtextxy(152,150,"Hai");
+    delay(700);
+    setcolor(BROWN);
+    cleardevice();
+    outtextxy(154,150,"Tu");
+    
+    
+    delay(1500);
+	   cleardevice();
+       setcolor(MAGENTA);
+	   outtextxy(155,155,"jeet");
+	   delay(800);
+	   cleardevice();
+       setcolor(CYAN);
+	   outtextxy(150,155,"kay");
+	   delay(800);
+	   cleardevice();
+	   outtextxy(150,155,"karu");
+	   delay(800);
+	   setcolor(WHITE);
+	   cleardevice();
+	   outtextxy(150,155,"Bi");
+	   delay(800);
+	   setcolor(GREEN);
+	   cleardevice();
+	   outtextxy(150,155,"kiya");
+	   delay(1300);
+	   setcolor(RED);
+	   cleardevice();
+	   outtextxy(150,155,"Haar");
+	   
+    setcolor(YELLOW);
+    delay(800);
+    cleardevice();
+    outtextxy(152,150,"Hai");
+    delay(800);
+    setcolor(RED);
+    cleardevice();
+    outtextxy(154,150,"Tu");
+    delay(800);
+	   outtextxy(150,155,"Woh");
+    setcolor(GREEN);
+    delay(900);
+    cleardevice();
+    outtextxy(152,150,"Hai");
+    delay(800);
+    setcolor(LIGHTRED);
+    cleardevice();
+    outtextxy(154,150,"Tu...");
+    
+    
+    
+    
+    
+    setcolor(DARKGRAY);
+     delay(70);
+      settextstyle(3,0,7);
+       cleardevice();
+	   outtextxy(50,250,"UNEEB ULLAH");
+	   delay(200);
+
+    
+    
+    
+    getch();
+
+    
+    closegraph();
+}
+//by memes by uneeb
